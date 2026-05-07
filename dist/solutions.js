540e02c84c1cffeddb0ed3969157e968e14098d8
Somen Singha <somensingha18@gmail.com>
2026-05-07 19:39:20 +0600

    Proble 1 solved

---
+++ dist/solutions.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function filterEvenNumbers(numbers) {
+    return numbers.filter((num) => num % 2 === 0);
+}
+console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
+//# sourceMappingURL=solutions.js.map
